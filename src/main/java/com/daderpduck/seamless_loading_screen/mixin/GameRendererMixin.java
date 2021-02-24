00dbc2d9b6a14b6024281182631198be2ea41a30
DaDerpDuck <14254403+DaDerpDuck@users.noreply.github.com>
2021-02-23 18:07:27 -0800

    Added text during holding duration

--- src/main/java/com/daderpduck/seamless_loading_screen/mixin/GameRendererMixin.java
+++ src/main/java/com/daderpduck/seamless_loading_screen/mixin/GameRendererMixin.java
@@ -3,14 +3,18 @@ package com.daderpduck.seamless_loading_screen.mixin;
 import com.daderpduck.seamless_loading_screen.ScreenshotLoader;
 import com.daderpduck.seamless_loading_screen.ScreenshotRenderer;
 import com.daderpduck.seamless_loading_screen.config.Config;
+import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.AbstractGui;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.util.text.TranslationTextComponent;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameRenderer.class)
@@ -18,8 +22,15 @@ public class GameRendererMixin {
     @Shadow
     @Final
     private Minecraft mc;
+    private MatrixStack matrixStack;
 
     private static float timePassed = 0;
+
+    @Redirect(method = "updateCameraAndRender(FJZ)V", at = @At(value = "NEW", target = "com/mojang/blaze3d/matrix/MatrixStack"))
+    private MatrixStack getMatrixStack() {
+        matrixStack = new MatrixStack();
+        return matrixStack;
+    }
 
     @Inject(method = "updateCameraAndRender(FJZ)V", at = @At("TAIL"))
     private void doFade(float partialTicks, long nanoTime, boolean renderWorldIn, CallbackInfo ci) {
@@ -30,10 +41,16 @@ public class GameRendererMixin {
             float alpha = Math.min(1F - (timePassed - holdTime)/fadeTime, 1F);
 
             if (alpha > 0) {
+                int scaledHeight = mc.getMainWindow().getScaledHeight();
+                int scaledWidth = mc.getMainWindow().getScaledWidth();
+
                 RenderSystem.enableBlend();
                 RenderSystem.defaultBlendFunc();
-                ScreenshotRenderer.renderScreenshot(mc.getMainWindow().getScaledHeight(), mc.getMainWindow().getScaledWidth(), (int)(alpha*255));
+                ScreenshotRenderer.renderScreenshot(scaledHeight, scaledWidth, (int)(alpha*255));
                 RenderSystem.disableBlend();
+
+                if (timePassed < holdTime)
+                    AbstractGui.drawCenteredString(matrixStack, mc.fontRenderer, new TranslationTextComponent("multiplayer.downloadingTerrain"), scaledWidth/2,70,0xFFFFFF);
 
                 timePassed += partialTicks;
             } else {

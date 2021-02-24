b45dccf9f55308b51e505a81cd98f863124e64eb
DaDerpDuck <14254403+DaDerpDuck@users.noreply.github.com>
2021-02-24 10:22:36 -0800

    Take screenshot on minecraft shutdown

--- src/main/java/com/daderpduck/seamless_loading_screen/mixin/MinecraftMixin.java
+++ src/main/java/com/daderpduck/seamless_loading_screen/mixin/MinecraftMixin.java
@@ -1,18 +1,24 @@
 package com.daderpduck.seamless_loading_screen.mixin;
 
-import com.daderpduck.seamless_loading_screen.ScreenshotLoader;
+import com.daderpduck.seamless_loading_screen.ScreenshotTaker;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screen.WorkingScreen;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
 public class MinecraftMixin {
-    @Inject(method = "loadWorld(Ljava/lang/String;)V", at = @At("HEAD"))
-    private void loadWorld(String worldName, CallbackInfo ci) {
-        ScreenshotLoader.setScreenshotWorld(worldName);
+    @Shadow
+    private volatile boolean running;
+
+    @Inject(method = "shutdown()V", at = @At("HEAD"), cancellable = true)
+    private void shutdown(CallbackInfo ci) {
+        if (Minecraft.getInstance().world != null) {
+            ScreenshotTaker.takeScreenshot(mc -> running = false);
+
+            ci.cancel();
+        }
     }
 }

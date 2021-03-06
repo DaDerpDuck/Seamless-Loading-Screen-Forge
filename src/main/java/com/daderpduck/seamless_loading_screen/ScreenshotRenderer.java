2eb8ab76bae20be396692405609c393bb85f664c
DaDerpDuck <14254403+DaDerpDuck@users.noreply.github.com>
2021-03-06 11:53:12 -0800

    Removed unused methods

--- src/main/java/com/daderpduck/seamless_loading_screen/ScreenshotRenderer.java
+++ src/main/java/com/daderpduck/seamless_loading_screen/ScreenshotRenderer.java
@@ -1,30 +1,13 @@
 package com.daderpduck.seamless_loading_screen;
 
 import com.daderpduck.seamless_loading_screen.config.Config;
-import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import org.lwjgl.opengl.GL11;
 
 public class ScreenshotRenderer {
-    public static void renderScreenBackground(Screen screen, MatrixStack stack) {
-        renderScreenBackground(screen, stack, 255);
-    }
-
-    public static void renderScreenBackground(Screen screen, MatrixStack stack, int alpha) {
-        if (!ScreenshotLoader.isLoaded()) {
-            screen.renderBackground(stack);
-            return;
-        }
-
-        renderScreenshot(screen.height, screen.width, alpha);
-
-        net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(new net.minecraftforge.client.event.GuiScreenEvent.BackgroundDrawnEvent(screen, new MatrixStack()));
-    }
-
     public static void renderScreenshot(double screenHeight, double screenWidth, int alpha) {
         Minecraft mc = Minecraft.getInstance();
         float imageRatio = ScreenshotLoader.getImageRatio();

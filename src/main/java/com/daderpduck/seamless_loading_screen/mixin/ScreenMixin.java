35b58cb5435b7d5302cfeb90fa75d994a8234978
DaDerpDuck <14254403+DaDerpDuck@users.noreply.github.com>
2021-03-06 11:46:31 -0800

    Fix some screens displaying dirt instead of the screenshot

--- src/main/java/com/daderpduck/seamless_loading_screen/mixin/ScreenMixin.java
+++ src/main/java/com/daderpduck/seamless_loading_screen/mixin/ScreenMixin.java
@@ -7,23 +7,22 @@ import net.minecraft.client.gui.screen.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Screen.class)
 public abstract class ScreenMixin {
     @Shadow public int height;
     @Shadow public int width;
 
-    @Shadow public abstract void renderDirtBackground(int vOffset);
-
-    @Redirect(method = "renderBackground(Lcom/mojang/blaze3d/matrix/MatrixStack;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;renderDirtBackground(I)V"))
-    private void renderDirtBackground(Screen screen, int vOffset) {
+    @Inject(method = "renderDirtBackground(I)V", at = @At("HEAD"), cancellable = true)
+    private void renderDirtBackground(int vOffset, CallbackInfo ci) {
         if (ScreenshotLoader.isLoaded()) {
+            Screen screen = (Screen) (Object) this;
             ScreenshotRenderer.renderScreenshot(this.height, this.width, 255);
 
             net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(new net.minecraftforge.client.event.GuiScreenEvent.BackgroundDrawnEvent(screen, new MatrixStack()));
-        } else {
-            this.renderDirtBackground(vOffset);
+            ci.cancel();
         }
     }
 }

e95e3005caa65e4482fade21f95dbc1247e9f5b2
DaDerpDuck <14254403+DaDerpDuck@users.noreply.github.com>
2021-03-12 19:00:17 -0800

    Changed screenshot to be taken upon world unload Removed redundant screenshot takers

--- src/main/java/com/daderpduck/seamless_loading_screen/mixin/MinecraftMixin.java
+++ src/main/java/com/daderpduck/seamless_loading_screen/mixin/MinecraftMixin.java
@@ -3,6 +3,8 @@ package com.daderpduck.seamless_loading_screen.mixin;
 import com.daderpduck.seamless_loading_screen.ScreenshotLoader;
 import com.daderpduck.seamless_loading_screen.ScreenshotTaker;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.registry.DynamicRegistries;
 import net.minecraft.world.WorldSettings;
 import net.minecraft.world.gen.settings.DimensionGeneratorSettings;
@@ -12,22 +14,35 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import javax.annotation.Nullable;
+
 @Mixin(Minecraft.class)
-public class MinecraftMixin {
-    @Shadow
-    private volatile boolean running;
+public abstract class MinecraftMixin {
+    private static boolean takenScreenshot = false;
 
-    @Inject(method = "shutdown()V", at = @At("HEAD"), cancellable = true)
-    private void shutdown(CallbackInfo ci) {
-        if (Minecraft.getInstance().world != null) {
-            ScreenshotTaker.takeScreenshot(mc -> running = false);
+    @Shadow public abstract void unloadWorld(Screen screenIn);
 
-            ci.cancel();
-        }
-    }
+    @Shadow @Nullable public ClientWorld world;
+
+    @Shadow protected abstract void runGameLoop(boolean renderWorldIn);
 
     @Inject(method = "createWorld(Ljava/lang/String;Lnet/minecraft/world/WorldSettings;Lnet/minecraft/util/registry/DynamicRegistries$Impl;Lnet/minecraft/world/gen/settings/DimensionGeneratorSettings;)V", at = @At("HEAD"))
     private void createWorld(String worldName, WorldSettings worldSettings, DynamicRegistries.Impl dynamicRegistriesIn, DimensionGeneratorSettings dimensionGeneratorSettings, CallbackInfo ci) {
         ScreenshotLoader.setScreenshotWorld(worldName);
+    }
+
+    @Inject(method = "unloadWorld(Lnet/minecraft/client/gui/screen/Screen;)V", at = @At("HEAD"), cancellable = true)
+    private void unloadWorldScreenshot(Screen screenIn, CallbackInfo ci) {
+        if (!takenScreenshot && world != null) {
+            ScreenshotTaker.takeScreenshot(mc -> {
+                System.out.println("took screenshot!");
+                takenScreenshot = true;
+                unloadWorld(screenIn);
+            });
+            runGameLoop(true);
+            ci.cancel();
+        } else {
+            takenScreenshot = false;
+        }
     }
 }

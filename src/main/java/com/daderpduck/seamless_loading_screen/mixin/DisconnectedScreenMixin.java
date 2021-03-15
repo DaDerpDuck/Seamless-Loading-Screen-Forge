cbd894d06dd56f4faed43fde2f9a2f5e66249496
DaDerpDuck <14254403+DaDerpDuck@users.noreply.github.com>
2021-03-15 15:54:13 -0700

    Fixed screenshot being rendered over dirt after failing to connect to a server Added a mixin to the disconnection screen that disables screenshot rendering, issue probably exists on realms too

---
+++ src/main/java/com/daderpduck/seamless_loading_screen/mixin/DisconnectedScreenMixin.java
@@ -0,0 +1,16 @@
+package com.daderpduck.seamless_loading_screen.mixin;
+
+import com.daderpduck.seamless_loading_screen.ScreenshotLoader;
+import net.minecraft.client.gui.screen.DisconnectedScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(DisconnectedScreen.class)
+public class DisconnectedScreenMixin {
+    @Inject(method = "init()V", at = @At("HEAD"))
+    private void init(CallbackInfo ci) {
+        ScreenshotLoader.resetState();
+    }
+}

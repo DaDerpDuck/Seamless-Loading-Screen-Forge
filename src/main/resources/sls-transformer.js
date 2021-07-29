81513085843fd2f2774f82d5a761cc2b1efbf5a5
DaDerpDuck <14254403+DaDerpDuck@users.noreply.github.com>
2021-07-28 20:27:45 -0700

    Convert to srg names

--- src/main/resources/sls-transformer.js
+++ src/main/resources/sls-transformer.js
@@ -15,7 +15,7 @@ function initializeCoreMod() {
             'target': {
                 'type': 'METHOD',
                 'class': 'net.minecraft.client.Minecraft',
-                'methodName': ASMAPI.mapMethod('createLevel'),
+                'methodName': ASMAPI.mapMethod('m_91202_'),
                 'methodDesc': '(Ljava/lang/String;Lnet/minecraft/world/level/LevelSettings;Lnet/minecraft/core/RegistryAccess$RegistryHolder;Lnet/minecraft/world/level/levelgen/WorldGenSettings;)V'
             },
             'transformer': function(methodNode) {
@@ -32,7 +32,7 @@ function initializeCoreMod() {
         	'target': {
         		'type': 'METHOD',
         		'class': 'net.minecraft.client.Minecraft',
-        		'methodName': ASMAPI.mapMethod('clearLevel'),
+        		'methodName': ASMAPI.mapMethod('m_91320_'),
         		'methodDesc': '(Lnet/minecraft/client/gui/screens/Screen;)V'
         	},
         	'transformer': function(methodNode) {
@@ -44,7 +44,7 @@ function initializeCoreMod() {
         			new JumpInsnNode(Opcodes.IFEQ, skip),
         			new VarInsnNode(Opcodes.ALOAD, 0),
         			new InsnNode(Opcodes.ICONST_1),
-        			ASMAPI.buildMethodCall("net/minecraft/client/Minecraft", ASMAPI.mapMethod("runTick"), "(Z)V", ASMAPI.MethodType.VIRTUAL),
+        			ASMAPI.buildMethodCall("net/minecraft/client/Minecraft", ASMAPI.mapMethod("m_91383_"), "(Z)V", ASMAPI.MethodType.VIRTUAL),
 					new InsnNode(Opcodes.RETURN),
 					skip
         		);
@@ -57,14 +57,14 @@ function initializeCoreMod() {
         	'target': {
 				'type': 'METHOD',
 				'class': 'net.minecraft.client.gui.screens.worldselection.WorldSelectionList$WorldListEntry',
-				'methodName': ASMAPI.mapMethod('loadWorld'),
+				'methodName': ASMAPI.mapMethod('m_101744_'),
 				'methodDesc': '()V'
 			},
 			'transformer': function(methodNode) {
 				var list = ASMAPI.listOf(
 					new VarInsnNode(Opcodes.ALOAD, 0),
-					new FieldInsnNode(Opcodes.GETFIELD, "net/minecraft/client/gui/screens/worldselection/WorldSelectionList$WorldListEntry", ASMAPI.mapField("summary"), "Lnet/minecraft/world/level/storage/LevelSummary;"),
-					ASMAPI.buildMethodCall("net/minecraft/world/level/storage/LevelSummary", ASMAPI.mapMethod("getLevelId"), "()Ljava/lang/String;", ASMAPI.MethodType.VIRTUAL),
+					new FieldInsnNode(Opcodes.GETFIELD, "net/minecraft/client/gui/screens/worldselection/WorldSelectionList$WorldListEntry", ASMAPI.mapField("f_101695_"), "Lnet/minecraft/world/level/storage/LevelSummary;"),
+					ASMAPI.buildMethodCall("net/minecraft/world/level/storage/LevelSummary", ASMAPI.mapMethod("m_78358_"), "()Ljava/lang/String;", ASMAPI.MethodType.VIRTUAL),
 					ASMAPI.buildMethodCall("com/daderpduck/seamless_loading_screen/events/Transformer", "postPreLoadLevel", "(Ljava/lang/String;)V", ASMAPI.MethodType.STATIC)
 				);
 				methodNode.instructions.insert(list);
@@ -76,7 +76,7 @@ function initializeCoreMod() {
         	'target': {
 				'type': 'METHOD',
 				'class': 'net.minecraft.client.MouseHandler',
-				'methodName': ASMAPI.mapMethod('turnPlayer'),
+				'methodName': ASMAPI.mapMethod('m_91523_'),
 				'methodDesc': '()V'
 			},
 			'transformer': function(methodNode) {
@@ -87,10 +87,10 @@ function initializeCoreMod() {
 					new JumpInsnNode(Opcodes.IFEQ, skip),
 					new VarInsnNode(Opcodes.ALOAD, 0),
 					new InsnNode(Opcodes.DCONST_0),
-					new FieldInsnNode(Opcodes.PUTFIELD, "net/minecraft/client/MouseHandler", ASMAPI.mapField("accumulatedDX"), "D"),
+					new FieldInsnNode(Opcodes.PUTFIELD, "net/minecraft/client/MouseHandler", ASMAPI.mapField("f_91516_"), "D"),
 					new VarInsnNode(Opcodes.ALOAD, 0),
 					new InsnNode(Opcodes.DCONST_0),
-					new FieldInsnNode(Opcodes.PUTFIELD, "net/minecraft/client/MouseHandler", ASMAPI.mapField("accumulatedDY"), "D"),
+					new FieldInsnNode(Opcodes.PUTFIELD, "net/minecraft/client/MouseHandler", ASMAPI.mapField("f_91517_"), "D"),
 					new InsnNode(Opcodes.RETURN),
 					skip
 				);
@@ -103,14 +103,14 @@ function initializeCoreMod() {
         	'target': {
 				'type': 'METHOD',
 				'class': 'net.minecraft.client.gui.screens.LevelLoadingScreen',
-				'methodName': ASMAPI.mapMethod('renderChunks'),
+				'methodName': ASMAPI.mapMethod('m_96149_'),
 				'methodDesc': '(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/server/level/progress/StoringChunkProgressListener;IIII)V'
 			},
 			'transformer': function(methodNode) {
 				var iterator = methodNode.instructions.iterator();
 				while (iterator.hasNext()) {
 					var insnNode = iterator.next();
-					if (insnNode.getOpcode() === Opcodes.INVOKESTATIC && insnNode.owner === "net/minecraft/client/gui/screens/LevelLoadingScreen" && insnNode.name === ASMAPI.mapMethod("fill") && insnNode.desc === "(Lcom/mojang/blaze3d/vertex/PoseStack;IIIII)V") {
+					if (insnNode.getOpcode() === Opcodes.INVOKESTATIC && insnNode.owner === "net/minecraft/client/gui/screens/LevelLoadingScreen" && insnNode.name === ASMAPI.mapMethod("m_93172_") && insnNode.desc === "(Lcom/mojang/blaze3d/vertex/PoseStack;IIIII)V") {
 						insnNode.owner = "com/daderpduck/seamless_loading_screen/events/Transformer";
 						insnNode.name = "changeChunkLoadFill";
 					}
@@ -123,7 +123,7 @@ function initializeCoreMod() {
         	'target': {
 				'type': 'METHOD',
 				'class': 'net.minecraft.world.level.storage.LevelStorageSource$LevelStorageAccess',
-				'methodName': ASMAPI.mapMethod('deleteLevel'),
+				'methodName': ASMAPI.mapMethod('m_78311_'),
 				'methodDesc': '()V'
 			},
 			'transformer': function(methodNode) {
@@ -139,7 +139,7 @@ function initializeCoreMod() {
 				if (tail) {
 					var list = ASMAPI.listOf(
 						new VarInsnNode(Opcodes.ALOAD, 0),
-						new FieldInsnNode(Opcodes.GETFIELD, "net/minecraft/world/level/storage/LevelStorageSource$LevelStorageAccess", ASMAPI.mapField("levelPath"), "Ljava/nio/file/Path;"),
+						new FieldInsnNode(Opcodes.GETFIELD, "net/minecraft/world/level/storage/LevelStorageSource$LevelStorageAccess", ASMAPI.mapField("f_78271_"), "Ljava/nio/file/Path;"),
 						ASMAPI.buildMethodCall("com/daderpduck/seamless_loading_screen/events/Transformer", "postDeleteSave", "(Ljava/nio/file/Path;)V", ASMAPI.MethodType.STATIC)
 					);
 					methodNode.instructions.insertBefore(tail, list);
@@ -152,7 +152,7 @@ function initializeCoreMod() {
 			'target': {
 				'type': 'METHOD',
 				'class': 'com.mojang.realmsclient.RealmsMainScreen',
-				'methodName': ASMAPI.mapMethod('play'),
+				'methodName': ASMAPI.mapMethod('m_86515_'),
 				'methodDesc': '(Lcom/mojang/realmsclient/dto/RealmsServer;Lnet/minecraft/client/gui/screens/Screen;)V'
 			},
 			'transformer': function(methodNode) {
